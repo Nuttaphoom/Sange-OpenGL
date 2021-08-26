96251e498020f08e005b3721ee08fafc122b84a7
LAPTOP-TFKNT2RT\Asus <nuttaphoomboonmee@hotmail.com>
2021-08-26 12:54:22 +0700

    Create WorkOrga.cpp
    //Testing

---
+++ WorkOrga.cpp
@@ -0,0 +1,5 @@
+//This is basically just a test file, along with some note for work organization. 
+
+//Arm will be handleing the character of player, ability, and the direction of the game.
+//Yean will be doing graphic stuffs, world interaction, map, rendering. 
+//Win will program AIs.
