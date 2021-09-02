66e7a5ac3a50cea63c20f1d7508249c7eab81d6f
LAPTOP-TFKNT2RT\Asus <nuttaphoomboonmee@hotmail.com>
2021-09-02 20:49:38 +0700

    Update WorkOrga.cpp

--- WorkOrga.cpp
+++ WorkOrga.cpp
@@ -3,3 +3,5 @@
 //Arm will be handleing the character of player, ability, and the direction of the game.
 //Yean will be doing graphic stuffs, world interaction, map, rendering. 
 //Win will program AIs.
+
+//TEST PUSHING 
