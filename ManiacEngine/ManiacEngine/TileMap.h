33cf1eeb908e2d7c58d778543343d5d8a9a0a6c8
LAPTOP-TFKNT2RT\Asus <nuttaphoomboonmee@hotmail.com>
2021-10-15 11:15:55 +0700

    Tilemap

--- ManiacEngine/ManiacEngine/TileMap.h
+++ ManiacEngine/ManiacEngine/TileMap.h
@@ -9,11 +9,13 @@ class TileMap  {
  		vector<vector<SpriteObject*>> tiles;
 	public : 
 		TileMap(int width, int height ,int** Mapdata, string texture_path, int rowMax, int columnMax) {
-			for (int i = 0; i < width; i++) {
+			width -= 1;
+			height -= 1; 
+
+			for (int i = 0; i < width + 1; i++) {
 				tiles.push_back(vector<SpriteObject*>());
-				for (int j = 0; j < height; j++) {
-					std::cout << "get here " << i << " , " << j << std::endl; 
-					tiles[i].push_back(new SpriteObject(texture_path, rowMax, columnMax));
+				for (int j = 0; j < height + 1; j++) {
+ 					tiles[i].push_back(new SpriteObject(texture_path, rowMax, columnMax));
 					
 					tiles[i][j]->SetAnimationLoop(0, 0, 0, 0);
 
@@ -21,22 +23,31 @@ class TileMap  {
 					tiles[i][j]->SetSize(64, -64);
 
  					/// Set position of each tile 
-					tiles[i][j]->SetPosition(glm::vec3(-GameEngine::GetInstance()->GetWindowWidth() / 2 + i * 64 + 32 , GameEngine::GetInstance()->GetWindowHeight() / 2 - j * 64 - 32, 1));
+					tiles[i][j]->SetPosition(glm::vec3(-GameEngine::GetInstance()->GetWindowWidth() / 2 + j * 64 + 32 , GameEngine::GetInstance()->GetWindowHeight() / 2 - i * 64 - 32, 1));
  					/// Calculate UV 
 					float uvs[8] ; 
  					
 					//Need to use Mapdata[i][j] for UV // UNFINISH !!!
-					uvs[0] = (	(1 / width) ) ;
-					uvs[1] = (	(1 / height) * (height - Mapdata[i][j])	);
+					float widthEachCell = 1.0f / (float)   width  ;
+					float heightEachCell = 1.0f / (float)  height   ;
+
+					cout << "widthEachCell : " << widthEachCell << endl;
+
+					uvs[0] = widthEachCell * Mapdata[i][j];		
+					uvs[1] = (height - ( Mapdata[i][j] / width)) * (heightEachCell) - 1 ;
+
+					uvs[2] = (widthEachCell * Mapdata[i][j]) + widthEachCell;
+					uvs[3] = (height - (Mapdata[i][j] / width)) * (heightEachCell)-1;
+
+					uvs[4] = (widthEachCell * Mapdata[i][j]) + widthEachCell;
+					uvs[5] = (height - (Mapdata[i][j] / width)) * (heightEachCell) ;
+
+					uvs[6] = (widthEachCell * Mapdata[i][j])  ;
+					uvs[7] = (height - (Mapdata[i][j] / width)) * (heightEachCell) ;
 					
-					uvs[2] = (	(1 / width) * Mapdata[i][j]) + 1 / width;
-					uvs[3] = (	(1 / height) * (height - Mapdata[i][j])	);
-
-					uvs[4] = (	(1 / width) * Mapdata[i][j]) + 1 / width;
-					uvs[5] = (	(1 / height) * (height - Mapdata[i][j]) + 1 / height);
-
-					uvs[6] = (	(1 / width) * Mapdata[i][j]);
-					uvs[7] = (	(1 / height) * (height - Mapdata[i][j]) + 1 / height);
+					cout << uvs[6] << "," << uvs[7] << "\t" << uvs[4] << "," << uvs[5] << endl;
+					cout << uvs[0] << "," << uvs[1] << "\t" << uvs[2] << "," << uvs[3] << endl;
+					cout << endl << endl << endl;
 
 					tiles[i][j]->SetUV(uvs);
 				}
