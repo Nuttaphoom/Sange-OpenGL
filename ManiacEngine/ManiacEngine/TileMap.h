6766e99746fd988b2756005c21150a7b40bdf22f
LAPTOP-TFKNT2RT\Asus <nuttaphoomboonmee@hotmail.com>
2022-02-03 11:45:22 +0700

    External bug fixes

--- ManiacEngine/ManiacEngine/TileMap.h
+++ ManiacEngine/ManiacEngine/TileMap.h
@@ -11,7 +11,7 @@ class TileMap  {
 	public : 
 		TileMap(int width, int height,int** FrontMiddleMapData ,int** MiddleMapdata,int** BackGroundMapDat, int** ColMapdata, string texture_path, int rowMax, int columnMax);
 		~TileMap(); 
- 
+		void SetUV(float widthEachCell, float heightEachCell, int columnMax, int rowMax, int i, int j, int** Mapdata,float uvs[8]);
 		vector<vector<SpriteObject*>> GetTiles(); 
 		vector<InvisibleObject*> GetColTiles();
 		
